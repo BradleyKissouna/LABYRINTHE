3836c4d4b79bdba6ba210bbf0945fad654876853
Bradley Kissouna <kissouna.b@gmail.com>
2025-03-31 16:01:39 +0200

    Add ladders

--- LABYRINTHE_SEQ.cpp
+++ LABYRINTHE_SEQ.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,6 +11,7 @@ struct Position {
 
 vector<vector<string>> mazes;
 Position startPos, endPos;
+vector<Position> ladders;
 bool collected[3] = {false, false, false}; // C, B, E
 
 // Directions possibles (haut, bas, gauche, droite)
@@ -39,7 +41,7 @@ void loadLabyrinths(const string &filename) {
         }
     }
     
-    // Ajouter le dernier labyrinthe s'il y en a un en attente
+    // Ajouter le dernier labyrinthe
     if (!currentLab.empty()) {
         mazes.push_back(currentLab);
     }
@@ -58,10 +60,94 @@ void displayLabyrinths() {
     }
 }
 
+// Vérifier si une position est valide
+bool isValidMove(int x, int y, int level) {
+    return (x >= 0 && x < mazes[level].size() &&
+            y >= 0 && y < mazes[level][0].size() &&
+            mazes[level][x][y] != '#');
+}
+
+// Fonction récursive de backtracking
+bool solveMaze(int x, int y, int level, int ladderIndex = 0) {
+    if (x == endPos.x && y == endPos.y && collected[0] && collected[1] && collected[2]) {
+        return true; // Fin atteinte avec tous les objets
+    }
+
+    char temp = mazes[level][x][y];
+    mazes[level][x][y] = '*'; // Marquer la case visitée
+
+    // Vérifier si un objet a été collecté
+    for (int i = 0; i < 3; i++) {
+        if (temp == OBJECTS[i]) collected[i] = true;
+    }
+
+    // Vérifier si on est sur une échelle
+    if (isdigit(temp)) {
+        int ladderNum = temp - '0';
+        if (ladderIndex < ladders.size() && ladderNum == ladderIndex + 1) {
+            ladderIndex++; // On valide cette échelle et passe à la suivante
+            cout << "Échelle " << ladderNum << " atteinte !\n";
+        }
+    }
+
+    // Exploration dans les 4 directions
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if (isValidMove(nx, ny, level) && solveMaze(nx, ny, level, ladderIndex)) {
+            return true;
+        }
+    }
+    
+    mazes[level][x][y] = temp;
+    return false;
+}
+
 int main() {
     loadLabyrinths("./labyrinthe.txt");
 
-    displayLabyrinths();
+    // displayLabyrinths();
+    // Traiter chaque labyrinthe génériquement, sans boucle prédéfinie
+    while (size_t mazeIndex = 0 < mazes.size()) {
+        vector<string> currentMaze = mazes[mazeIndex];
+        bool foundStart = false, foundEnd = false;
+        ladders.clear();
+
+        // Initialisation de la position de départ et d'arrivée pour chaque labyrinthe
+        for (int i = 0; i < currentMaze.size(); i++) {
+            for (int j = 0; j < currentMaze[i].size(); j++) {
+                if (currentMaze[i][j] == 'D') {
+                    startPos = {i, j};  // Utiliser startPos ici
+                    foundStart = true;
+                }
+                if (currentMaze[i][j] == 'A') {
+                    endPos = {i, j};  // Utiliser endPos ici
+                    foundEnd = true;
+                }
+                if (isdigit(currentMaze[i][j])) {  // Vérifier si c'est une échelle
+                    ladders.push_back({i, j});
+                }
+            }
+        }
+
+        // Trier les échelles par ordre croissant
+        sort(ladders.begin(), ladders.end(), [&](Position a, Position b) {
+            return currentMaze[a.x][a.y] < currentMaze[b.x][b.y];
+        });
+
+        if (foundStart && foundEnd) {
+            cout << "Labyrinthe " << mazeIndex + 1 << " en cours de résolution...\n";
+            
+            if (solveMaze(startPos.x, startPos.y, mazeIndex)) {
+                cout << "Solution trouvée pour le labyrinthe " << mazeIndex + 1 << " !\n";
+            } else {
+                cout << "Aucun chemin trouvé pour le labyrinthe " << mazeIndex + 1 << ".\n";
+            }
+        } else {
+            cout << "Départ ou arrivée manquants dans le labyrinthe " << mazeIndex + 1 << ".\n";
+        }
+        // Passer au labyrinthe suivant
+        mazeIndex++;
+    }
 
     return 0;
 }
